8544f3dfc27fc671ffb0679a0f6d6350baa35943
awallathome <adam.wallis@me.com>
2018-02-24 11:45:17 -0500

    nav bar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+    return <div className="App">
+        <nav class="white" role="navigation">
+          <div class="nav-wrapper container">
+            <ul class="toprow">
+              <li>
+                <a href="index.html" class="brand-logo">
+                  <span id="logo-container">Adam Wallis</span>
+                </a>
+              </li>
+              <li id="recentprojects">
+                <a href="portfolio.html">
+                  <span>Recent Projects</span>
+                </a>
+              </li>
+              <li id="resume">
+                <a href="https://www.dropbox.com/s/3pukaawyxzkm85h/AdamWallis_Resume%20-%20Google%20Docs.pdf?dl=0" target="_blank">
+                  <span>Resume</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
+      </div>;
   }
 }
 
