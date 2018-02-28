ace5987c16ce187e9490d8f99b2a032dbb5f0ebb
awallathome <adam.wallis@me.com>
2018-02-27 19:57:15 -0500

    minor changes to sandwich.js

--- src/Components/Sandwich.js
+++ src/Components/Sandwich.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import './Sandwich.css';
 
+function openSandwich(e) {
+  e.preventDefault();
+
+  // here is where I need to change the styling of the 'hidden' div below
+  ("#hidden").display = 'block';
+}
+
 
 class Sandwich extends Component {
   render () {
-    return <div className="sandwichesMenu" onClick="openSandwich()">
-        <div className="sandwiches" />
-        <div className="sandwiches" />
-        <div className="sandwiches" />
-        <span className="sandwichItems">Recent Projects</span>
-        <span className="sandwichItems">Resume</span>
-      </div>;
+    return <div className="sandwichesMenu" id="menu" onClick={openSandwich}>
+      <div className="sandwich" />
+      <div className="sandwich" />
+      <div className="sandwich" />
+      <div className="sandwichItemsDiv" id="hidden">
+        <p className="sandwichItems sandwichRecentProjects">Recent Projects</p>
+        <p className="sandwichItems sandwichResume">Resume</p>
+      </div>
+    </div>;
   }
 };
 
